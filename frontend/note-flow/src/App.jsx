c8371507cda72b28e605b0afb4c15ef3fc9afd94
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-03-06 22:53:19 +0530

    cretae intial react frontend with tailwindcss

---
+++ frontend/note-flow/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-red-500 text-2xl'>
+      hellow 
+    </h1>
+    </>
+  )
+}
+
+export default App
