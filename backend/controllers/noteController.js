bf5e7b1d2d49aa80c80029942708831c4ecf807d
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-03-08 00:35:57 +0530

    create route and logic for fetch notes

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -4,12 +4,11 @@ import NoteModel from "../models/noteModel.js";
 const createNote = async (req, res) => {
   try {
     const { title, content } = req.body;
-    const  userId  = req.user.id;
-
+    const userId = req.user.id;
     if (!title || !content) {
       return res.status(400).json({ success: false, message: "Title and content are required" });
     }
-    
+
     const note = await NoteModel.create({ title, content, userId });
     return res.status(201).json({ success: true, note, message: "Note created successfully" });
   } catch (error) {
@@ -18,4 +17,25 @@ const createNote = async (req, res) => {
   }
 };
 
-export { createNote }
+// get All notes
+const getMyNotes = async (req, res) => {
+  try {
+
+    const userId = req.user.id;
+    const notes = await NoteModel.find({ userId }).sort({ createdAt: -1 });
+
+    res.json({
+      success: true,
+      notes
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Failed to fetch notes"
+    });
+  }
+};
+
+export { createNote, getMyNotes }
--- backend/routes/noteRoute.js
+++ backend/routes/noteRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createNote } from '../controllers/noteController.js'
+import { createNote, getMyNotes } from '../controllers/noteController.js'
 import authUser from '../middleware/authMiddleware.js'
 
 const noteRouter = express.Router()
 
 noteRouter.post('/create',authUser,createNote)
+noteRouter.get("/my-notes", authUser, getMyNotes);
 
 export default noteRouter
