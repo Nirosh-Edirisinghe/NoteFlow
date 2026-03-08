63863bdfc74fb5b9df8008bd1296cbd0a05ebedd
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-03-08 16:15:15 +0530

    chnage the styles of add note page

--- frontend/note-flow/src/components/NoteEditor.jsx
+++ frontend/note-flow/src/components/NoteEditor.jsx
@@ -1,12 +1,18 @@
 import React, { useContext, useState } from 'react'
 import { useEditor, EditorContent } from "@tiptap/react";
+import { Bold, Italic, Strikethrough, Code, Type, List, ListOrdered, Eye, Undo, Redo, Hash } from "lucide-react";
 import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
 import axios from "axios";
 import { toast } from 'react-toastify'
 import { AppContext } from '../context/AppContext';
 
 const extensions = [
-  StarterKit
+  StarterKit,
+  Placeholder.configure({
+    placeholder: 'Start writing your note...',
+    showOnlyWhenEditable: true
+  }),
 ]
 
 const content = ``
@@ -56,169 +62,148 @@ const NoteEditor = () => {
   };
 
   return (
-    <div className="p-4">
-      <div className='flex gap-2 mb-4 flex-wrap'>
-        <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          disabled={!editor.can().chain().focus().toggleBold().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('bold') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          B
-        </button>
+    <div className="h-screen flex flex-col">
+      <div className="p-4 bg-white sticky top-0 z-10">
+        <div className='flex gap-2 mb-4 flex-wrap'>
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            disabled={!editor.can().chain().focus().toggleBold().run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('bold') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            <Bold size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          disabled={!editor.can().chain().focus().toggleItalic().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('italic') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          <em>I</em>
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            disabled={!editor.can().chain().focus().toggleItalic().run()}
+            className={`px-3 py-1 border  border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('italic') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            <Italic size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          disabled={!editor.can().chain().focus().toggleStrike().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('strike') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          <s>S</s>
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            disabled={!editor.can().chain().focus().toggleStrike().run()}
+            className={`px-3 py-1 border  border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('strike') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            <Strikethrough size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          disabled={!editor.can().chain().focus().toggleCode().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('code') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          code
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleCode().run()}
+            disabled={!editor.can().chain().focus().toggleCode().run()}
+            className={`px-3 py-1 border  border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('code') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            <Code size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          disabled={!editor.can().chain().focus().toggleCodeBlock().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('codeBlock') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          codeBlock
-        </button>
+          <button
+            onClick={() => editor.chain().focus().setParagraph().run()}
+            className={`px-3 py-1 border  border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('paragraph') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            P
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().setParagraph().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('paragraph') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          P
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+            className={`px-3 py-1 border  border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('heading', { level: 1 }) ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            H1
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('heading', { level: 1 })
-            ? 'bg-gray-600 text-white'
-            : 'bg-white text-black'
-            }`}
-        >
-          H1
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('heading', { level: 2 }) ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            H2
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('heading', { level: 2 }) ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          H2
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('heading', { level: 3 }) ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            H3
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('heading', { level: 3 }) ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          H3
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('heading', { level: 4 }) ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            H4
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('heading', { level: 4 }) ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          H4
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('heading', { level: 5 }) ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            H5
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('heading', { level: 5 }) ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          H5
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('heading', { level: 6 }) ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            H6
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('heading', { level: 6 }) ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          H6
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('bulletList') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            <List size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('bulletList') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          UL
-        </button>
+          <button
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            className={`px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 ${editor?.isActive('orderedList') ? 'bg-gray-300 text-gray-700' : 'bg-white text-gray-700'}`}
+          >
+            <ListOrdered size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('orderedList') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          OL
-        </button>
+          <button
+            onClick={() => editor.chain().focus().setHorizontalRule().run()}
+            className="px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 bg-white text-gray-700"
+          >
+            HR
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={`px-3 py-1 border rounded ${editor.isActive('blockquote') ? 'bg-gray-600 text-white' : 'bg-white text-black'}`}
-        >
-          Quote
-        </button>
+          <button
+            onClick={() => editor.chain().focus().setHardBreak().run()}
+            className="px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 bg-white text-gray-700"
+          >
+            Break
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().setHorizontalRule().run()}
-          className="px-3 py-1 border rounded bg-white text-black"
-        >
-          HR
-        </button>
+          <button
+            onClick={() => editor.chain().focus().undo().run()}
+            // disabled={!editor.can().chain().focus().undo().run()}
+            className="px-3 py-1 border border-gray-300 rounded cursor-pointer hover:bg-gray-100 bg-white text-gray-700 disabled:opacity-50"
+          >
+            <Undo size={16} />
+          </button>
 
-        <button
-          onClick={() => editor.chain().focus().setHardBreak().run()}
-          className="px-3 py-1 border rounded bg-white text-black"
-        >
-          Break
-        </button>
-
-        <button
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().chain().focus().undo().run()}
-          className="px-3 py-1 border rounded bg-white text-black disabled:opacity-50"
-        >
-          Undo
-        </button>
-
-        <button
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().chain().focus().redo().run()}
-          className="px-3 py-1 border rounded bg-white text-black disabled:opacity-50"
-        >
-          Redo
-        </button>
-
+        </div>
       </div>
 
-      {/* Title Input */}
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Enter note title..."
-        className="w-full mb-4 p-2 border rounded focus:outline-none focus:ring focus:border-blue-500"
-      />
+      <div className="flex-1 overflow-y-auto p-4">
+        {/* Title Input */}
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter Note Title..."
+          className="w-full mb-4 p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-gray-300"
+        />
 
-      <div className="prose max-w-none border rounded p-4 min-h-75 bg-white">
-        <EditorContent editor={editor} />
+        <div className="prose max-w-none border border-gray-300 shadow-md rounded-lg p-4 min-h-75 bg-white">
+          <EditorContent editor={editor} />
+        </div>
+
+        <button onClick={createNote} className="mt-4 px-8 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-600">
+          Save Note
+        </button>
       </div>
 
-      <button onClick={createNote} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-        Save
-      </button>
 
     </div>
   )
--- frontend/note-flow/src/pages/AddNote.jsx
+++ frontend/note-flow/src/pages/AddNote.jsx
@@ -4,9 +4,9 @@ import NoteEditor from '../components/NoteEditor'
 const AddNote = () => {
   return (
     <div className='px-1 py-6 bg-white rounded-xl sm:p-6 min-h-screen'>
-      <div className="min-h-screen bg-gray-100 py-8">
+      <div className="min-h-screen py-8">
         <div className="max-w-4xl mx-auto">
-          <h1 className="text-3xl font-bold mb-6 text-center">Add Your notes</h1>
+          <h1 className="text-3xl font-bold mb-6 text-center text-gray-700">Add Your notes</h1>
           <NoteEditor />
         </div>
       </div>
